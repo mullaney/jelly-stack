8995e9d0a363baefec54569a46ad0fdf62806bc8
Kevin Kelly <kvinklly@gmail.com>
2020-06-28 11:38:04 -0500

    Add handlebars templates

--- build.js
+++ build.js
@@ -4,21 +4,27 @@ const showdown = require('showdown')
 const converter = new showdown.Converter()
 const pagesDir = 'pages'
 const distDir = 'dist'
+const Handlebars = require('handlebars')
+const indexHtml = fs.readFileSync( 'templates/index.html', 'utf-8' )
+const pageTemplate = Handlebars.compile(indexHtml)
 
 // make a dist folder if it doesn't exist
-if (!fs.existsSync(distDir)){
+if (!fs.existsSync(distDir)) {
   fs.mkdirSync(distDir);
 }
 
 // Get a list of all files in pages dir
 const pages = glob.sync(pagesDir + '/**/*.md')
 
+const newFilename = file => {
+  const pathParts = file.split('/')
+  const fileName = pathParts[pathParts.length - 1].split('.md')[0]
+  return fileName + '.html'
+}
+
 pages.forEach(file => {
   //get base name
-  const parts = file.split('/')
-  const filename = parts[parts.length - 1]
-  const newfilename = filename.split('.md')[0] + '.html'
   const markdown = fs.readFileSync( file, 'utf-8' )
-  const html = converter.makeHtml(markdown)
-  fs.writeFileSync(`${distDir}/${newfilename}`, html)
+  const html = pageTemplate({ content: converter.makeHtml(markdown) })
+  fs.writeFileSync(`${distDir}/${newFilename(file)}`, html)
 })
