37420b3fe11f9866a451c69fbc64ad32f2828f49
Kevin Kelly <kvinklly@gmail.com>
2020-10-10 15:44:55 -0500

    Update styleing and move vars closer to execute

--- build.js
+++ build.js
@@ -1,29 +1,16 @@
 const fs = require('fs')
 const glob = require('glob')
 const showdown = require('showdown')
-const converter = new showdown.Converter()
-const pagesDir = 'pages'
-const distDir = 'dist'
-const Handlebars = require('handlebars')
-const applicationHtml = fs.readFileSync( 'templates/application.html', 'utf-8' )
-const mainHtml = fs.readFileSync( 'templates/main.html', 'utf-8' )
-const styleLink = fs.readFileSync( 'templates/style_link.html', 'utf-8' )
-const applicationTemplate = Handlebars.compile(applicationHtml)
-const mainTemplate = Handlebars.compile(mainHtml)
-const styleLinkTemplate = Handlebars.compile(styleLink)
-const newCssFiles = []
-const { loopedTemplateRender } = require('./src/util/templateRender.js')
 
 // Make directories
 const { makeDirectories, newFilename, newCssFilename } = require('./src/util/fileServices.js')
 makeDirectories(fs, ['dist', 'dist/css'])
-const Markdown = require('./src/markdown.js')
-const Metadata = require('./src/metadata.js')
-
 
 // Get a list of all files in pages dir
+const pagesDir = 'pages'
 const pages = glob.sync(pagesDir + '/**/*.md')
 const cssFiles = glob.sync('assets/css/**/*.css')
+const newCssFiles = []
 
 cssFiles.forEach(cssFile => {
   const pathParts = cssFile.split('/')
@@ -43,12 +30,26 @@ cssFiles.forEach(cssFile => {
   }
 })
 
+const Markdown = require('./src/markdown.js')
+const Metadata = require('./src/metadata.js')
+const Handlebars = require('handlebars')
+
+const styleLinkHtml = fs.readFileSync( 'templates/style_link.html', 'utf-8' )
+const styleLinkTemplate = Handlebars.compile(styleLinkHtml)
 const cssFilesData = newCssFiles.map( function(f) { return { cssFilename: f } })
+const { loopedTemplateRender } = require('./src/util/templateRender.js')
 const styleLinks = loopedTemplateRender(styleLinkTemplate, cssFilesData)
+
+const mainHtml = fs.readFileSync( 'templates/main.html', 'utf-8' )
+const mainTemplate = Handlebars.compile(mainHtml)
+
+const applicationHtml = fs.readFileSync( 'templates/application.html', 'utf-8' )
+const applicationTemplate = Handlebars.compile(applicationHtml)
 
 pages.forEach(file => {
   const markdown = (new Markdown(file)).load()
   const compiledMetadata = (new Metadata(markdown.metadata)).build().metadata()
+  const converter = new showdown.Converter()
   const content = converter.makeHtml(markdown.html())
   const mainHtml = mainTemplate({ content })
   const html = applicationTemplate({
@@ -57,5 +58,6 @@ pages.forEach(file => {
     title: markdown.metadata.title,
     metadata: compiledMetadata
   })
+  const distDir = 'dist'
   fs.writeFileSync(`${distDir}/${newFilename(file)}`, html)
 })
--- pages/index.md
+++ pages/index.md
@@ -1,11 +1,15 @@
 <!--
-  "title": "The New Kelly Chronicle",
-  "author": "Dee Kelly",
-  "description": "Stories about the Kellys",
-  "image": "https://www.state.gov/wp-content/uploads/2018/11/Ireland-2109x1406.jpg",
-  "published": "June 28, 2020"
+  "title": "The Jelly Stack",
+  "author": "Kevin kelly",
+  "description": "Publish static html/css sites from markdown",
+  "image": "./images/jelly.png",
+  "published": "October 10, 2020"
 -->
 
-# The New Kelly Chronicle!
+# The Jelly Stack
 
-Here is the [about page](/about.html).
+What is Jelly Stack? It's a super fast way to create a website. Edit the markdown, run `npm run build` and copy the contents of the dist folder to a webserver. You can automate the process using services like github pages or netlify to deploy in seconds.
+
+![Jelly](./images/jelly.png)
+
+Check out the [repo on github](https://github.com/mullaney/jelly-stack).
